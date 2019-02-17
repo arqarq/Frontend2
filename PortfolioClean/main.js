df793ce8856277003b52ab58ff6a165e24ae1f2e
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-17 22:46:25 +0100

    - main.js cleaned

--- PortfolioClean/main.js
+++ PortfolioClean/main.js
@@ -1,16 +1,14 @@
-window.addEventListener('DOMContentLoaded',
-    () => {
-        const accordeons = document.querySelectorAll('.accordeon__head');
-        // console.log(accordeons);
-        accordeons.forEach(accordeon => {
-            accordeon.addEventListener('click', () => {
-                // const isActive = accordeon.parentElement.classList.contains('active');
-                const active = document.querySelector('.accordeon.active');
-                accordeon.parentElement.classList.toggle('active');
-                if (active) {
-                    active.classList.remove('active')
-                }
-                // active.classList.remove('active');
-            })
+window.addEventListener('DOMContentLoaded', () => {
+    const accordeons = document.querySelectorAll('.accordeon__head');
+    // console.log(accordeons);
+    accordeons.forEach(accordeon => {
+        accordeon.addEventListener('click', () => {
+            // const isActive = accordeon.parentElement.classList.contains('active');
+            const active = document.querySelector('.accordeon.active');
+            accordeon.parentElement.classList.toggle('active');
+            if (active) {
+                active.classList.remove('active')
+            }
         })
-    });
+    })
+});
