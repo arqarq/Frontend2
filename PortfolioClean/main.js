a609e8d9a0f411b25de2c75a9b2c016057ff980b
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-17 14:58:52 +0100

    - main.js

--- PortfolioClean/main.js
+++ PortfolioClean/main.js
@@ -1,5 +1,13 @@
 window.addEventListener('DOMContentLoaded',
     () => {
         const accordeons = document.querySelectorAll('.accordeon__head');
-        console.log(accordeons);
+        // console.log(accordeons);
+        accordeons.forEach(accordeon => {
+            accordeon.addEventListener('click', event => {
+                const isActive = accordeon.parentElement.classList.contains('active');
+                const active = document.querySelector('.accordeon.active');
+                active.classList.remove('active');
+                accordeon.parentElement.classList.add('active')
+            })
+        })
     });
